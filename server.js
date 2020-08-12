9a373740212b20024e10069c242310b2b44f3452
viscorym <faiyaz_r@outlook.com>
2020-08-13 03:18:06 +0600

    step 2

---
+++ server.js
@@ -0,0 +1,43 @@
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+
+dotenv.config();
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+app.prepare().then(() => {
+    const server = new Koa();
+    server.use(session({ secure: true, sameSite: 'none' }, server));
+    server.keys = [SHOPIFY_API_SECRET_KEY];
+    server.use(
+        createShopifyAuth({
+            apiKey: SHOPIFY_API_KEY,
+            secret: SHOPIFY_API_SECRET_KEY,
+            scopes: ['read_products'],
+            afterAuth(ctx) {
+                const { shop, accessToken } = ctx.session;
+                ctx.redirect('/');
+            },
+        }),
+    );
+    server.use(verifyRequest());
+    server.use(async (ctx) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+        ctx.statusCode = 200;
+        return
+    });
+    server.listed(port, () => {
+        console.log(`> Ready of https://localhost:${port}`);
+    });
+});
